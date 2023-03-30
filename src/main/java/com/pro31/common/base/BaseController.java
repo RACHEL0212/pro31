95efec8f225109ff3182bd16244f85b4fd32d34e
RACHEL0212 <105160038+RACHEL0212@users.noreply.github.com>
2023-03-30 16:40:04 +0900

    Update BaseController.java

--- src/main/java/com/pro31/common/base/BaseController.java
+++ src/main/java/com/pro31/common/base/BaseController.java
@@ -1,26 +1,20 @@
 package com.pro31.common.base;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.pro31.goods.vo.ImageFileVO;
 
